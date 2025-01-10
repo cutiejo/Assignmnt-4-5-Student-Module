b65d6745b34b8959aa6292ccfad49e1469871564
cutiejo <jocatayoc15@gmail.com>
2025-01-10 13:08:35 +0800

    delete

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Put, Patch, Delete, Body, Param } from '@nestjs/common';
+import { StudentService } from './student.service';
+import { Student } from './student.entity';
 
-@Controller('student')
-export class StudentController {}
+@Controller('students')
+export class StudentController {
+  constructor(private readonly studentService: StudentService) {}
+
+
+  // DELETE
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.studentService.deleteStudent(id);
+  }
+}
